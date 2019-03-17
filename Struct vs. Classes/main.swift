4dac251ec92639798bbd3ab45337b0cb9668c84a
Konstantin <grahovsky@gmail.com>
2019-03-17 11:49:46 +0300

    Initial Commit

---
+++ Struct vs. Classes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Struct vs. Classes
+//
+//  Created by Konstantin on 17/03/2019.
+//  Copyright © 2019 Konstantin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
